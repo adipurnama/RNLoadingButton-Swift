6011b90c498be7fd30778a8fe3cb1d6b403dcab0
Adi Purnama <adi.purnama@alfacart.com>
2017-06-05 11:47:35 +0700

    fix right alignment

--- RNLoadingButton/RNLoadingButton.swift
+++ RNLoadingButton/RNLoadingButton.swift
@@ -386,8 +386,8 @@ open class RNLoadingButton: UIButton {
                 x = Float(self.frame.size.width) - ( Float(frame.size.width) + Float(self.activityIndicatorEdgeInsets.right) )
             } else {
                 // default to placing the indicator at 3/4 the buttons length, making sure it doesn't touch the button content
-                let contentRightEdge = (lengthOccupied / 2) + (Float(frame.width) / 2)
-                let threeFourthsButtonWidth = 3 * (Float(frame.width) / 4)
+                let contentRightEdge = (lengthOccupied / 2) + (Float(self.frame.width) / 2)
+                let threeFourthsButtonWidth = 3 * (Float(self.frame.width) / 4)
                 x = max(contentRightEdge, threeFourthsButtonWidth)
             }
             
